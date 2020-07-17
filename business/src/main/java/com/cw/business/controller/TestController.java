d0515f116123c806769f964acfc33a8cb3f6cdfc
chen_ts <13813641925@163.com>
2020-07-17 23:06:06 +0800

    增加大章表，并生成持久层代码

---
+++ business/src/main/java/com/cw/business/controller/TestController.java
@@ -0,0 +1,21 @@
+package com.cw.business.controller;
+
+import com.cw.server.domain.Test;
+import com.cw.server.service.TestService;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.annotation.Resource;
+import java.util.List;
+
+@RestController
+//@RequestMapping("/system")  //这种方法还是太低级了， 看配置文件context方式
+public class TestController {
+    @Resource
+    private TestService testService;
+
+    @RequestMapping("/hello")
+    public List<Test> test(){
+        return testService.list();
+    }
+}
